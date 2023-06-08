ee95955747eccd5cf0c99ae78b4686a002b1f0e3
enocklennox <lennoxkipngeno@gmail.com>
2023-06-08 21:08:32 +0300

    3. Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    result = 0
+    for i in range(1, argc + 1):
+        result += int(argv[i])
+    print(result)
