4fd3b68371a716190902cecb738476583b08c650
enocklennox <lennoxkipngeno@gmail.com>
2023-06-08 20:50:00 +0300

    1. My first toolbox!

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, subtract, multiply, divide
+
+a = 10
+b = 5
+
+result1 = add(a,b)
+result2 = subtract(a,b)
+result3 = multiply(a,b)
+result4 = divide(a,b)
+
+print(result1, result2, result3, result4)
