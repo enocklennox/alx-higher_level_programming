7c2ce09b2ebcf717a6c76353adad75223d7a37ef
enocklennox <lennoxkipngeno@gmail.com>
2023-06-12 21:59:29 +0300

    13. Linked list palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: pointer to head of list.
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
+ */
+int is_palindrome(listint_t **head)
+{
+	int i = 0, j = 0;
+	int arr[100000];
+	listint_t *temp = *head;
+
+	while (temp != NULL)
+	{
+		arr[i] = temp->n;
+		i++;
+		temp = temp->next;
+	}
+
+		for (j = 0; j < i / 2; j++)
+	{
+		if (arr[j] != arr[i - j - 1])
+		return (0);
+	}
+	return (1);
+}
